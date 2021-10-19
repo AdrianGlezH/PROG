24f00e1e233cf95190ab8f08265a7c07582ddcfc
AdrianGlezH <91198240+AdrianGlezH@users.noreply.github.com>
2021-10-19 09:50:26 +0200

    Add files via upload

---
+++ Boletin2_1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_1;
+
+
+public class Boletin2_1 {
+    
+    public static void main(String[] args) {
+    int base=4;
+    int altura=3;
+    System.out.println("El área de un trángulo de base 4 y altura 3 es =");
+    System.out.println(base*altura/2);
+    }
+    
+}
