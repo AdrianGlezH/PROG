84eb6df5ccc5dd04a0d9ecf961fe3cd96a9c6ffb
AdrianGlezH <91198240+AdrianGlezH@users.noreply.github.com>
2021-12-14 18:43:02 +0100

    Add files via upload

---
+++ boletin12/Boletin12.java
@@ -0,0 +1,15 @@
+package boletin12;
+
+public class Boletin12 {
+
+    public static void main(String[] args) {
+        
+        Ejer1 obj = new Ejer1();
+        obj.aparcar();
+        
+        // Ejercicio 2 del boletin 12
+        //
+        // En el anterior programa se visualizaría "8 9 9 ".Los cuales serían los valores que daría el sout del método modificar
+    }
+    
+}
