afb0637dd9e4e5283e3b92b84e2e528d00187ab2
AdrianGlezH <91198240+AdrianGlezH@users.noreply.github.com>
2022-03-13 22:59:01 +0100

    Add files via upload

---
+++ 2ªEvaluación/boletin23/Boletin23.java
@@ -0,0 +1,19 @@
+package boletin23;
+
+public class Boletin23 {
+
+    public static void main(String[] args) {
+
+         Metodos.lonxTexto();
+         Metodos.desmTexto();
+         Metodos.invertTexto();
+         Metodos.senEspazos();
+         Metodos.contarVeC();
+         Metodos.divTexto();
+         Metodos.transMai();
+         Metodos.compTexto();
+         Metodos.subVogais();
+         Metodos.transString();
+         Metodos.contCaract();
+    }
+}
---
+++ 2ªEvaluación/boletin23/Metodos.java
@@ -0,0 +1,106 @@
+package boletin23;
+
+import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
+
+public class Metodos {
+
+    public static void lonxTexto(){
+        
+        String texto = "Esto é  Java!";
+        //int lonx = texto.length();
+        
+        System.out.println("La longitud es de "+texto.length()+" caracteres.");
+    }
+    
+    public static void desmTexto(){
+        String texto = "Java";
+        String split[] = texto.split("");
+        for (int i=0; i<split.length; i++){
+            System.out.println(split[i]);
+        }
+    }
+    
+    public static void invertTexto(){
+        String texto = "Java desde 0";
+        String invertida = "";
+        for (int i=texto.length()-1; i >=0; i--){
+            invertida += texto.charAt(i);
+        }
+        System.out.println(invertida);
+    }
+    
+    public static void senEspazos(){
+        String texto = "James Gosling Created Xava";
+        System.out.println(texto.replaceAll("\\s", ""));
+    }
+    
+    public static void contarVeC(){
+        String texto = "java java java";
+        String sinEsp = texto.replaceAll("\\s", "");
+        int vocales = sinEsp.replaceAll("[aeiou]","").length();
+        System.out.println("La frase contiene "+vocales+" vocales");
+        String sinEsp2 = texto.replaceAll("\\s", "");
+        int consonantes = sinEsp2.replaceAll("[qwrtypsdfghjklñzxcvbnm]","").length();
+        System.out.println("La frase contienen "+consonantes+" consonantes");
+    }
+    
+    public static void divTexto(){
+        String texto = "www.javadesde0.com";
+        String parte = texto.substring(0, 8);
+        String parte2 = texto.substring(8, 18);
+        System.out.println(parte + "\n" + parte2);
+    }  
+    
+    public static void transMai(){
+        String textoMin = "javeros";
+        String textoMai = textoMin.toUpperCase();
+        System.out.println("Transformación a maiúsculas: "+textoMai);
+        System.out.println("Transformación a minúsculas: "+textoMai.toLowerCase());
+    }
+    
+    public static void compTexto(){
+        String text1 = "Java";
+        String text2 = "JavaScript";
+        if (text1.compareTo(text2) == 0)
+            System.out.println("Los textos son iguales.");
+        else 
+            System.out.println("Los textos no son iguales.");
+    }
+    
+    public static void subVogais(){
+        String texto = " Jeve jeve jeve";
+        for (int i=0; i<texto.length(); i++){
+            if (texto.charAt(i) == 'e'){
+                texto = texto.replace("e", "a");
+            }
+        }
+        System.out.println(texto);
+    }
+    
+    public static void transString(){
+        String caracter = " ABCD";
+        byte[] valor = caracter.getBytes(StandardCharsets.US_ASCII);
+        for (int i=0; i<valor.length; i++)
+            System.out.println("Valor en ASCII: " + valor[i]);
+    }
+    
+    public static void contCaract(){
+        String cadea = "Hola, soy alumno de DAM1, y soy programador desde el 2021";
+        int letra = 0;
+        int digitos = 0;
+        int espacios = 0;
+        for (int i=0; i<cadea.length(); i++){
+            if (cadea.charAt(i) >= 'a' && cadea.charAt(i) <= 'z' || cadea.charAt(i) >= 'A' && cadea.charAt(i) <= 'Z'){
+                letra++;
+            } else if (cadea.charAt(i) >= '0' && cadea.charAt(i) <= '9'){
+                digitos++;
+            } else if (cadea.charAt(i) == ' '){
+                espacios++;
+            }
+            
+        }
+        System.out.println("Nº letras: " + letra + "\nNº de dígitos: " + digitos + "\nNº de espacios: " + espacios);
+    }
+
+}
