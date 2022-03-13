ecfbe8bfdd5ed939fe08979bb78df250cfd150d6
AdrianGlezH <91198240+AdrianGlezH@users.noreply.github.com>
2022-03-13 22:47:53 +0100

    Add files via upload

---
+++ 2ªEvaluación/Boletin21_1/Boletin211.java
@@ -0,0 +1,12 @@
+package boletin21.pkg1;
+
+public class Boletin211 {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[6];
+
+        ArrayNumeros.arrayNumeros(numeros);
+        ArrayNumeros.invertirAmosar(numeros);
+    }
+    
+}
