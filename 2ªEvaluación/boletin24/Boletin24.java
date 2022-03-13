ddec1e557dff810ef5ce89aa462f7cc69b11b4fe
AdrianGlezH <91198240+AdrianGlezH@users.noreply.github.com>
2022-03-13 23:07:20 +0100

    Add files via upload

---
+++ 2ªEvaluación/boletin24/Boletin24.java
@@ -0,0 +1,35 @@
+package boletin24;
+
+import Libreria.Libreria;
+import java.util.ArrayList;
+import libreriaAdrian.lerDatos;
+
+public class Boletin24 {
+
+    public static void main(String[] args) {
+
+                ArrayList <Libros> lista = new ArrayList();
+        
+        int op; 
+        
+            do{
+                
+            op = lerDatos.lerInt(" **** MENU ****" + 
+            "\n0 --> Saír do programa \n1 --> Engadir libro \n2 --> Eliminar libro"
+            + "\n3 --> Amosar libro\n4 --> Dar de baixa nº=0\n5 --> Consultar libro" );
+            
+            switch (op){
+                case 1 : Libreria.engadir(lista);
+                        break;
+                case 2 : Libreria.eliminarLibros(lista);
+                        break;
+                case 3 : Libreria.amosarLibro(lista);
+                        break;
+                case 4 : Libreria.darDeBaixa(lista);
+                        break;
+                case 5 : Libreria.consultarLibro(lista);
+            } 
+            } while(op!=0);
+    }
+
+    }
