168e1b1618a8284beba236d9a1dcdc8d36173cf7
AdrianGlezH <91198240+AdrianGlezH@users.noreply.github.com>
2022-03-13 20:33:39 +0100

    Add files via upload

---
+++ 2ªEvaluación/Boletin18/Conversor.java
@@ -0,0 +1,17 @@
+
+package convertirtemperatura;
+
+public class Conversor {
+    
+    private final float excepcion = 80;
+    
+    public float centigradosAFharenheit(float tempcenti){
+        float Fharen = (9.0f/5.0f*tempcenti+32.4f);
+        return Fharen;
+    }
+    
+    public float centígradosAReamur(float tempcenti){
+        float Reamur = 4.0f/5.0f*tempcenti;
+        return tempcenti;
+    }
+}
---
+++ 2ªEvaluación/Boletin18/ConvertirTemperatura.java
@@ -0,0 +1,9 @@
+package convertirtemperatura;
+
+public class ConvertirTemperatura {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
